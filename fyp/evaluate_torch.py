7a181aa2e4ba191bb1a9c45c4477b871b8dbe9f2
junyittcxt <junyitt@cxtanalytics.com>
2020-06-09 10:12:53 -0400

    current setup

--- fyp/evaluate_torch.py
+++ fyp/evaluate_torch.py
@@ -1,12 +1,12 @@
 # CUDA_VISIBLE_DEVICES=1 python evaluate_torch.py
 import torch
 from tqdm import tqdm
-from pytorch_adacos.mnist import archs
-from pytorch_adacos.metrics import AdaCos
+from pytorch.mnist import archs
+from pytorch.metrics import AdaCos
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torchvision import models
-from pytorch_adacos.model_helper import select_model, select_metric
+from pytorch.model_helper import select_model, select_metric
 import numpy as np
 
 if torch.cuda.is_available():
@@ -18,18 +18,24 @@ train_name = f"{model_type}_v4"
 best = ""
 
 main_model_dir = "torch_models" #torch_models
-model_type = "resnet18"
-train_name = f"{model_type}_erase_v02"
+# model_type = "xception"
+# train_name = 'xception_d400_force_v2'
+model_type = "vgg11"
+train_name = "vgg_d400_force_linear_v1_v00"
+best = "best_avg_acc_"
 best = "best_acc_"
-best = ""
+size = 400
+batch_size = 8
+# best = ""
 
 metric_type = "adacos"
+metric_type = "softmax"
 model=select_model(model_type, {})
 
 PATH = f"./{main_model_dir}/{train_name}/{best}model.pth"
 PATH2 = f"./{main_model_dir}/{train_name}/{best}metric_fc.pth"
 
-metric_fc = select_metric(metric_type, num_ftr = 1000)
+metric_fc = select_metric(metric_type, num_ftr = 1000, num_classes=5)
 
 model.load_state_dict(torch.load(PATH))
 metric_fc.load_state_dict(torch.load(PATH2))
@@ -37,12 +43,13 @@ model.eval()
 metric_fc.eval()
 
 transform_test = transforms.Compose([
+        transforms.Resize((size, size)),
         transforms.ToTensor(),
     ])
-unseen_test_set = datasets.ImageFolder(root='./data3/test', transform=transform_test)
+unseen_test_set = datasets.ImageFolder(root='../all_train_300/test', transform=transform_test)
 unseen_test_loader = torch.utils.data.DataLoader(
         unseen_test_set,
-        batch_size=64,
+        batch_size=batch_size,
         shuffle=False,
         num_workers=8)
 
@@ -52,7 +59,10 @@ for i, (input, target) in tqdm(enumerate(unseen_test_loader), total=len(unseen_t
     input, target = input.to(device), target.to(device)
     # input, target2 = input.cuda(), target.cuda()
     feature = model(input)
-    output = metric_fc(feature, target)
+    if metric_type=="softmax":
+        output = metric_fc(feature)
+    else:
+        output = metric_fc(feature, target)
     y_pred += output.cpu().detach().numpy().argmax(axis = 1).tolist()
     y_true += target.cpu().numpy().tolist()
 y_true = np.array(y_true)
@@ -60,11 +70,13 @@ y_pred = np.array(y_pred)
 len(y_pred), len(y_true)
 
 from evaluate.metrics import *
-
+from custom_math.kappa import quadratic_kappa
 acc = accuracy(y_true, y_pred)
 avg = avg_acc(y_true, y_pred)
 cm = get_cm(y_true, y_pred)
+qk = quadratic_kappa(y_true, y_pred)
 get_dist(y_true, y_pred)
 print("Accuracy:", acc)
 print("Avg Accuracy:", avg)
+print("QK:", qk)
 print(cm)
--- fyp/train.sh
+++ fyp/train.sh
@@ -1,1 +1,2 @@
 CUDA_VISIBLE_DEVICES=0 python train_torch_yaml.py --config yaml_config/xception.yaml
+# CUDA_VISIBLE_DEVICES=1 python train_torch_yaml.py --config yaml_config/xception.yaml
