0450763aa1945491dff9bd98f901f04c60e0fbc0
junyittcxt <junyitt@cxtanalytics.com>
2020-06-16 09:31:12 -0400

    added return_5

--- fyp/pytorch/dual_data_helper.py
+++ fyp/pytorch/dual_data_helper.py
@@ -8,7 +8,7 @@ import joblib
 import os, glob
 import torch
 import pandas as pd
-from skimage import io, transform
+
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
@@ -16,6 +16,8 @@ from torchvision import transforms, utils
 
 from PIL import Image, ImageOps
 from PIL import Image
+from skimage import io, transform
+from preprocessing.image_preprocess import preprocess_eye
 
 def create_dual_label_df(main_data_dir = "../all_train_300", train_dir_list = ["full_train", "val"]):
     all_files = []
@@ -68,23 +70,27 @@ def split_dual_df(dual_df, p = 0.2, seed = 123):
     d1 = dual_df.drop(d2.index)
     return d1, d2 # train, val
 
-def open_image(img_name, transform = True):
+def open_image(img_name, transform_1 = False, transform_2 = True):
     image2 = Image.open(img_name)
-    new_size = image2.size
-    max_size = max(image2.size)
-    desired_size = max_size
-    delta_w = desired_size - new_size[0]
-    delta_h = desired_size - new_size[1]
-    padding = (delta_w//2, delta_h//2, delta_w-(delta_w//2), delta_h-(delta_h//2))
-    new_im = ImageOps.expand(image2, padding)
+    try:
+        if transform_2:
+            new_size = image2.size
+            max_size = max(image2.size)
+            desired_size = max_size
+            delta_w = desired_size - new_size[0]
+            delta_h = desired_size - new_size[1]
+            padding = (delta_w//2, delta_h//2, delta_w-(delta_w//2), delta_h-(delta_h//2))
+            new_im = ImageOps.expand(image2, padding)
+    except:
+        new_im = image2
     return new_im
 
 class DualImgDataset(Dataset):
-    def __init__(self, df, transform=None):
+    def __init__(self, df, transform=None, apply_raw_transform = False, return_5 = False):
         self.df = df.copy()
         self.transform = transform
-
-
+        self.apply_raw_transform = apply_raw_transform
+        self.return_5 = return_5
         self.targets = np.array(df["labels_x"].astype(int).values.tolist() + df["labels_y"].astype(int).values.tolist())
         self.n = self.df.shape[0]
 
@@ -112,10 +118,18 @@ class DualImgDataset(Dataset):
             label = self.df["labels_y"].values[idx]
             image_1 = open_image(img_name_1)
             image_2 = open_image(img_name_2)
+
+            # if self.apply_raw_transform:
+            #     pass
+
             if self.transform:
                 image_1 = self.transform(image_1)
                 image_2 = self.transform(image_2)
-        return image_1, image_2, int(label)
+        
+        if self.return_5:
+            return img_name_1, img_name_2, image_1, image_2, int(label)
+        else:
+            return image_1, image_2, int(label)
 
 
 def initialize_dual_gen(train_label_df, val_label_df, size, batch_size, reweight_sample = -1, reweight_sample_factor = 1, workers = 4):
@@ -154,11 +168,11 @@ def initialize_dual_gen(train_label_df, val_label_df, size, batch_size, reweight
     return train_gen, val_gen
 
 
-def create_data_loader(df, size, batch_size = 6, workers = 4):
+def create_data_loader(df, size, batch_size = 6, workers = 4, return_5 = False):
     transform_val = transforms.Compose([
             transforms.Resize((size, size)),
             transforms.ToTensor()
         ])
-    test_set = DualImgDataset(df, transform_val)
+    test_set = DualImgDataset(df, transform_val, return_5 = return_5)
     test_gen = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=workers)
     return test_gen
