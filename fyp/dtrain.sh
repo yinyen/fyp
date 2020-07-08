0face14984e647919f739c3a8659a7c76e2f1630
yinyen <yinyen9662@gmail.com>
2020-07-09 00:17:14 +0800

    fixed sh

--- fyp/dtrain.sh
+++ fyp/dtrain.sh
@@ -2,10 +2,10 @@
 # CUDA_VISIBLE_DEVICES=0 python train_dual.py --config yaml_config/new_dual_resnext.yaml
 
 # active learning
-# UI
+# Random
 # CUDA_VISIBLE_DEVICES=0 python train_dual_active.py --config yaml_config/new_al_random_resnext.yaml
 
-# Random
+# UI
 # CUDA_VISIBLE_DEVICES=0 python train_dual_active.py --config yaml_config/new_al_ui_resnext.yaml
 
 # active learning - demo
